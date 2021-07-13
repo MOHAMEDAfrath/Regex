c9ed92fdb385891683f121b94cdded52ba6c377f
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-13 12:16:59 +0530

    [Afrath] Updated the file with First name validation

--- Regex/Program.cs
+++ Regex/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 
-namespace Regex
+namespace RegexDemoOperations
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to regex");
+            Console.WriteLine("****First Name Validation****");
+            RegexDemo.CheckFirstName();
         }
     }
 }
---
+++ Regex/RegexDemo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexDemoOperations
+{
+    class RegexDemo
+    {
+        public static void CheckFirstName()
+        {
+            string[] firstName = { "Ram", "Afrath", "-Ram", "1Kumar", "R", "Mohamed","ashwik","A23rock","R  ","a{}2{D}","RA","Ar","RAM","Ramu"};
+            Regex regex = new Regex(@"^[A-Z][a-z]{2,}$");
+            for (int i = 0; i < firstName.Length; i++)
+            {
+                Match match = regex.Match(firstName[i]);
+                if (match.Success)
+                {
+                    Console.WriteLine(firstName[i]+" ----->Valid");
+                }
+                else
+                {
+                    Console.WriteLine(firstName[i] + " ----->Invalid");
+                }
+            }
+        }
+    }
+}
